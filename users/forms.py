6cd4b9751ab801b3242b629fcda83cc9d419d197
ebsouza <ebsouza07@yahoo.com>
2022-04-23 08:48:30 -0300

    add User forms

--- users/forms.py
+++ users/forms.py
@@ -3,4 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    pass
+    phone = forms.CharField(help_text='Required.')
+    institution = forms.CharField(help_text='Required.')
+    role = forms.CharField(help_text='Required.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'phone', 'institution', 'role', 'password1', 'password2', )
