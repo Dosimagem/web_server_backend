45adadc2c934702ab2f1682b4aa9f9f937d0c919
ebsouza <ebsouza07@yahoo.com>
2022-05-01 02:16:35 -0300

    update routes

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('auth-example', views.auth_example, name='auth-example'),
+    path('auth-example/', views.auth_example, name='auth-example'),
     path('accounts/signup/', views.signup, name='signup'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
