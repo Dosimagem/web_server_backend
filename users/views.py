70dc11b8f3d31a022cb2684c66e64a9d5959e11d
ebsouza <ebsouza07@yahoo.com>
2022-05-01 01:42:02 -0300

    fix signup view

--- users/views.py
+++ users/views.py
@@ -14,14 +14,18 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             
+            user.refresh_from_db()
             user.profile.phone = form.cleaned_data.get('phone')
             user.profile.institution = form.cleaned_data.get('institution')
             user.profile.role = form.cleaned_data.get('role')
+            user.save()
 
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+
+            return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
