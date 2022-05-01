dae1c4b72ebce906c5ed502907799d485be88fcc
ebsouza <ebsouza07@yahoo.com>
2022-05-01 01:45:22 -0300

    add User flow routes

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('auth-example', views.auth_example, name='auth-example'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
 ]
