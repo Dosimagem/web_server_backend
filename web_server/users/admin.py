c67966461f82cdc377ec6ac136cc845eddb288bf
Henrique Andrade <henrique.ccda@gmail.com>
2022-06-06 21:40:42 -0300

    Configurar e costumizar o admin do django.
    close #25

--- web_server/users/admin.py
+++ web_server/users/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import CostumUser
+
+
+@admin.register(CostumUser)
+class CostumUser(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'institution', 'role', 'is_active', 'is_staff', 'date_joined')
