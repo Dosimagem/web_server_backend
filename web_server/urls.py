436a6ee5129e0bfb4e978a7fb044a9593f9b5601
Henrique Andrade <henrique.ccda@gmail.com>
2022-06-06 21:40:42 -0300

    Configurar e costumizar o admin do django.
    close #25

--- web_server/urls.py
+++ web_server/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dosimagem/admin/', admin.site.urls),
     path('', include('web_server.users.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
+
+
+admin.site.site_header = 'Dosimagem adminstrator'
