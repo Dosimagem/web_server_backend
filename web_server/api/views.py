6b745f12f17aef71173e20970889581299ea00d6
Henrique Andrade <henrique.ccda@gmail.com>
2022-08-27 23:55:08 -0300

    Rota para obter dados de usuários. (#89)
    close #89

--- web_server/api/views.py
+++ web_server/api/views.py
@@ -2,13 +2,23 @@ from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (
+                                        api_view,
+                                        authentication_classes,
+                                        permission_classes
+                                        )
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 
 from web_server.core.forms import SignupForm
 
 User = get_user_model()
+
+
+class MyTokenAuthentication(TokenAuthentication):
+    keyword = 'Bearer'
 
 
 @api_view(['POST'])
@@ -41,6 +51,27 @@ class MyObtainAuthToken(ObtainAuthToken):
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({'id': user.uuid, 'token': token.key, 'is_staff': user.is_staff})
+
+
+@api_view(['GET'])
+@authentication_classes([MyTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def users(request, id):
+
+    if user := User.objects.filter(uuid=id).first():
+        return Response(data=_user_to_dict(user))
+
+    return Response({}, status=HTTPStatus.NOT_FOUND)
+
+
+def _user_to_dict(user):
+    return dict(
+        name=user.profile.name,
+        phone=user.profile.phone,
+        role=user.profile.role,
+        institution=user.profile.institution,
+        email=user.email
+    )
 
 
 def _list_errors(errors):
