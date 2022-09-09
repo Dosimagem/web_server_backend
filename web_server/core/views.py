918f3e0f5de37ef37995943eb4c844f75e8f9673
Henrique Andrade <henrique.ccda@gmail.com>
2022-09-08 23:51:12 -0300

    Mundando a pagina index

--- web_server/core/views.py
+++ web_server/core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    return JsonResponse({'message': 'Welcome to Dosiamge API !'})
