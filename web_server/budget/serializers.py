bf8a22333408de62d70cd3a8434d6d3695f37547
Henrique Andrade <henrique.ccda@gmail.com>
2022-11-05 22:05:20 -0300

    Rota para o orçamanto da mecânica computacional #298
    close #298

--- web_server/budget/serializers.py
+++ web_server/budget/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
 
-class BudgetSerilizer(serializers.Serializer):
+class GeneralBudgetSerilizer(serializers.Serializer):
     service = serializers.CharField(max_length=30)
     treatment_type = serializers.CharField(max_length=60)
     number_of_samples = serializers.IntegerField(min_value=0)
     frequency = serializers.IntegerField(min_value=0)
     comments = serializers.CharField(max_length=100000)
+
+
+class CompModelBudgetSerilizer(serializers.Serializer):
+    service = serializers.CharField(max_length=30)
+    research_line = serializers.CharField(max_length=60)
+    project_description = serializers.CharField(max_length=100000)
--- web_server/budget/views.py
+++ web_server/budget/views.py
@@ -6,20 +6,25 @@ from django.template.loader import render_to_string
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from web_server.budget.serializers import BudgetSerilizer
+from web_server.budget.serializers import (
+    CompModelBudgetSerilizer,
+    GeneralBudgetSerilizer,
+)
+from web_server.core.decorators import user_from_token_and_user_from_url
 
 DOSIMAGEM_EMAIL = settings.DEFAULT_FROM_EMAIL
 
 
 @api_view(['POST'])
-def send_email(request, user_id):
+@user_from_token_and_user_from_url
+def general_budget_mail(request, user_id):
 
     user = request.user
 
     if user.email_not_verified():
         return Response(data={'error': 'O usuario não teve o email verificado ainda.'}, status=HTTPStatus.CONFLICT)
 
-    serializer = BudgetSerilizer(data=request.data)
+    serializer = GeneralBudgetSerilizer(data=request.data)
 
     if not serializer.is_valid():
         return Response(data={'error': serializer.errors}, status=HTTPStatus.BAD_REQUEST)
@@ -27,7 +32,30 @@ def send_email(request, user_id):
     context = serializer.data
     context['user'] = user
 
-    body = render_to_string('budget/email.txt', context)
+    body = render_to_string('budget/general_budget.txt', context)
+
+    send_mail('Pedido de Orçamento', body, DOSIMAGEM_EMAIL, [DOSIMAGEM_EMAIL])
+
+    return Response(status=HTTPStatus.NO_CONTENT)
+
+
+@api_view(['POST'])
+@user_from_token_and_user_from_url
+def comp_model_budget_mail(request, user_id):
+    user = request.user
+
+    if user.email_not_verified():
+        return Response(data={'error': 'O usuario não teve o email verificado ainda.'}, status=HTTPStatus.CONFLICT)
+
+    serializer = CompModelBudgetSerilizer(data=request.data)
+
+    if not serializer.is_valid():
+        return Response(data={'error': serializer.errors}, status=HTTPStatus.BAD_REQUEST)
+
+    context = serializer.data
+    context['user'] = user
+
+    body = render_to_string('budget/comp_model_budget.txt', context)
 
     send_mail('Pedido de Orçamento', body, DOSIMAGEM_EMAIL, [DOSIMAGEM_EMAIL])
 
