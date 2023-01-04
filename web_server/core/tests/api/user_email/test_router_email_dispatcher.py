63768da99e63038bee37d8f48fae70666573f4d9
Henrique Andrade <henrique.ccda@gmail.com>
2023-01-03 23:29:37 -0300

    Modificando os test_allowed_methods #351
    - Usando set para fazer a verificações dos métodos
    
    close #351

--- web_server/core/tests/api/user_email/test_router_email_dispatcher.py
+++ web_server/core/tests/api/user_email/test_router_email_dispatcher.py
@@ -30,10 +30,9 @@ def test_allowed_method(client_api_auth, user):
 
     assert resp.status_code == HTTPStatus.OK
 
-    options = resp.headers['Allow'].split(',')
+    options = map(str.strip, resp.headers['Allow'].split(','))
 
-    for o in options:
-        assert o.strip() in ['OPTIONS', 'PATCH', 'GET']
+    assert set(options) == set(['OPTIONS', 'PATCH', 'GET'])
 
 
 def test_update_email_token_id_and_user_id_dont_match(client_api_auth, user):
